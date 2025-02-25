56c0d7256f5b1211388b5fd5e1f32b8504d0f5f5
AyeshaNasirWebDeveloper <ayeshanasir806@gmail.com>
2025-02-25 22:40:44 +0500

    Modules

---
+++ Modules/server.js
@@ -0,0 +1,18 @@
+// *************************** COMMMON JS ******************************
+// const { sum, sub, mul, div } = require("./utils")
+
+// console.log(`Add two Numbers = ${sum(10,20)}`)
+// console.log(`sub two Numbers = ${sub(10,20)}`)
+// console.log(`mul two Numbers = ${mul(10,20)}`)
+// console.log(`div two Numbers = ${div(10,20)}`)
+
+
+
+// ***************************** ES Lint ********************************
+
+import { sum, sub, mul, div } from "./utils.js" 
+
+console.log(`Add two Numbers = ${sum(10,20)}`)
+console.log(`sub two Numbers = ${sub(10,20)}`)
+console.log(`mul two Numbers = ${mul(10,20)}`)
+console.log(`div two Numbers = ${div(10,20)}`)
---
+++ Modules/utils.js
@@ -0,0 +1,15 @@
+// ******************************** COMMON JS *****************************
+// const sum = ( a , b ) => a + b
+// const sub = ( a , b ) => a - b
+// const mul = ( a , b ) => a * b
+// const div = ( a , b ) => a / b
+
+// module.exports = { sum , sub , mul , div}
+
+
+// ********************************* ES Lint *******************************
+
+export const sum = ( a , b ) => a + b
+export const sub = ( a , b ) => a - b
+export const mul = ( a , b ) => a * b
+export const div = ( a , b ) => a / b
